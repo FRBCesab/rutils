f8f4ac23000ef36d02fbe07a686a41e909d4fbcf
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-04-25 10:51:13 +0200

    feat: add len function

---
+++ R/len.R
@@ -0,0 +1,16 @@
+#' A shortcut for the function length
+#'
+#' @param x an R object.
+#'
+#' @return See ?length for a complete description.
+#' 
+#' @export
+#'
+#' @examples
+#' length(letters)
+#' 
+#' len(letters)
+
+len <- function(x) {
+  length(x)
+}
