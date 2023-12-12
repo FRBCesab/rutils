0d175465ab2550768d865c30fb8d03d1ffbd081d
Nicolas Casajus <nicolas.casajus@gmail.com>
2023-12-12 09:28:43 +0100

    tests: adapt tests (new column)

--- tests/testthat/test-address_to_coords.R
+++ tests/testthat/test-address_to_coords.R
@@ -34,16 +34,19 @@ test_that("Function address_to_coords() - Test for success", {
   
   expect_equal(class(x), "data.frame")
   expect_equal(nrow(x), 1L)
-  expect_equal(ncol(x), 2L)
+  expect_equal(ncol(x), 3L)
   
   expect_true(colnames(x)[1] == "lon")
   expect_true(colnames(x)[2] == "lat")
+  expect_true(colnames(x)[3] == "name")
   
   expect_equal(class(x[ , 1]), "numeric")
   expect_equal(class(x[ , 2]), "numeric")
+  expect_equal(class(x[ , 3]), "character")
   
   expect_equal(x[1, "lon"], -95.3677, tolerance = 0.0001)
   expect_equal(x[1, "lat"],  29.75894, tolerance = 0.0001)
+  expect_equal(x[1, "name"],  "Houston, Harris County, Texas, United States")
   
   
   ## Bad address ----
@@ -54,11 +57,13 @@ test_that("Function address_to_coords() - Test for success", {
   
   expect_equal(class(x), "data.frame")
   expect_equal(nrow(x), 0L)
-  expect_equal(ncol(x), 2L)
+  expect_equal(ncol(x), 3L)
   
   expect_true(colnames(x)[1] == "lon")
   expect_true(colnames(x)[2] == "lat")
+  expect_true(colnames(x)[3] == "name")
   
   expect_equal(class(x[ , 1]), "numeric")
   expect_equal(class(x[ , 2]), "numeric")
+  expect_equal(class(x[ , 3]), "character")
 })
